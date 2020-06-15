c8f8d3fb871ceae3cbf8dc0d43bfb20479eec83c
dnldnt <dnl.d80@gmail.com>
2020-06-15 14:07:47 +0430

    the first commit from danial

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!!!");
         System.out.println("danial is a big idiot!");
+        System.out.println("I know it's true but why?");
     }
 }

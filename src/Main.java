75d748c249904fec06c534ccbeb5c513afa90139
Asus <alirezasakhaeirad@gmail.com>
2020-06-17 08:49:14 +0430

    sss

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
+        Node[] nodes = new Node[100];
+        for (int i=0;i<100;i++){
+            nodes[i] = new Node();
+            nodes[i].setUnion(i);
+        }
+
+
 
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -3,8 +3,28 @@ import java.util.HashMap;
 public class Node {
     private double voltage;
 
+    private int union;
+    private boolean added;
+
     public Node(){
         voltage=0;
+        added = false;
+    }
+
+    public int getUnion() {
+        return union;
+    }
+
+    public void setUnion(int union) {
+        this.union = union;
+    }
+
+    public boolean isAdded() {
+        return added;
+    }
+
+    public void setAdded(boolean added) {
+        this.added = added;
     }
 
     void setVoltage(double voltage){

c90b252e49b7131175c9349b55220cd711c49aa9
Asus <alirezasakhaeirad@gmail.com>
2020-07-15 07:56:44 +0430

    sss

--- src/InputManager.java
+++ src/InputManager.java
@@ -2,10 +2,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class InputManager{
+public class InputManager {
     File input;
     Circuit circuit;
-    boolean flagTran=false, isInputValid = true;
+    boolean flagTran = false, isInputValid = true, isEveryNumberValid = true;
 
     InputManager(File input) {
         this.input = input;
@@ -17,73 +17,88 @@ public class InputManager{
         ArrayList<String> inputLines = new ArrayList<String>(0);
         try {
             Scanner inputScanner = new Scanner(input);
-            while (inputScanner.hasNextLine() && isInputValid) {
-                isInputValid = false;
+            while (inputScanner.hasNextLine() && isInputValid && isEveryNumberValid) {
                 String string = inputScanner.nextLine();
                 string = string.trim();
-                inputLines.add(string);
-                char firstLetter = string.charAt(0);
-                switch (firstLetter) {
-                    case 'd':
-                        setD(circuit,string);
-                        break;
-                    case 'I':
-                        addIndependentSource(circuit, string, 'I');
-                        break;
-                    case 'V':
-                        addIndependentSource(circuit, string, 'V');
-                        break;
-                    case 'R':
-                        addRLC(circuit, string, 'R');
-                        break;
-                    case 'C':
-                        addRLC(circuit, string, 'C');
-                        break;
-                    case 'L':
-                        addRLC(circuit, string, 'L');
-                        break;
-                    case 'F':
-                        addCurrentDependent(circuit, string, 'I');
-                        break;
-                    case 'H':
-                        addCurrentDependent(circuit, string, 'V');
-                        break;
-                    case 'G':
-                        addVoltageDependent(circuit, string, 'I');
-                        break;
-                    case 'E':
-                        addVoltageDependent(circuit, string, 'V');
-                        break;
-                    // adding Ideal Diode
-                    case 'D':
-                        addDiode(circuit,string);
-                        break;
-                    case '.':
-                        Scanner scannerTran = new Scanner(string);
-                        if (scannerTran.next().equals(".tran")){
-                            circuit.setMaximumTime(unitCalculator(scannerTran.next()));
-                            flagTran = true;
-                        }
-                    case '*':
-                        break;
+                isEveryNumberValid = isEveryNumberValid(string);
+                if (isEveryNumberValid) {
+                    isInputValid = false;
+                    inputLines.add(string);
+                    char firstLetter = string.charAt(0);
+                    switch (firstLetter) {
+                        case 'd':
+                            isInputValid = setD(circuit, string);
+                            break;
+
+                        case 'R':
+                            isInputValid = addRLC(circuit, string, 'R');
+                            break;
+                        case 'C':
+                            isInputValid = addRLC(circuit, string, 'C');
+                            break;
+                        case 'L':
+                            isInputValid = addRLC(circuit, string, 'L');
+                            break;
+
+                        case 'I':
+                            isInputValid = addIndependentSource(circuit, string, 'I');
+                            break;
+                        case 'V':
+                            isInputValid = addIndependentSource(circuit, string, 'V');
+                            break;
+
+                        case 'F':
+                            isInputValid = addCurrentDependent(circuit, string, 'I');
+                            break;
+                        case 'H':
+                            isInputValid = addCurrentDependent(circuit, string, 'V');
+                            break;
+                        case 'G':
+                            isInputValid = addVoltageDependent(circuit, string, 'I');
+                            break;
+                        case 'E':
+                            isInputValid = addVoltageDependent(circuit, string, 'V');
+                            break;
+                        // adding Ideal Diode
+                        case 'D':
+                            isInputValid = addDiode(circuit, string);
+                            break;
+                        case '.':
+                            Scanner scannerTran = new Scanner(string);
+                            if (scannerTran.next().equals(".tran")) {
+                                circuit.setMaximumTime(unitCalculator(scannerTran.next()));
+                                flagTran = true;
+                            }
+                        case '*':
+                            isInputValid = true;
+                            break;
+                    }
+                }
+                else {
+                    System.out.println("(invalid number)There is a problem found in line " + inputLines.size() + 1);
                 }
             }
+            if (!isInputValid) {
+                System.out.println("(invalid input)There is a problem found in line " + inputLines.size());
+            }
         } catch (Exception e) {
-            System.out.println("There is a problem found in line " + inputLines.size());
+            System.out.println("(Exception)There is a problem found in line " + inputLines.size());
         }
 
         return circuit;
     }
 
-    public void setD(Circuit circuit,String string){
+
+
+    public boolean setD(Circuit circuit, String string) {
         Scanner scanner = new Scanner(string);
         System.out.println(string);
         char vdi = string.charAt(1);
         scanner.next();
         double dAmount = unitCalculator(scanner.next());
-      //  if (dAmount <= 0)
-//            return false;
-        switch (vdi){
+          if (dAmount <= 0)
+            return false;
+        switch (vdi) {
             case 'v':
                 circuit.setDv(dAmount);
                 break;
@@ -94,20 +109,50 @@ public class InputManager{
                 circuit.setDt(dAmount);
                 break;
             default:
-//                return false;
+                return false;
         }
-//        return true;
-    }
-    public boolean isTran(){
-        return flagTran;
+        return true;
     }
 
     //methods to add an element to the circuit
-    public static void addIndependentSource(Circuit circuit, String string, int IV) {
+    public static boolean addRLC(Circuit circuit, String string, int RLC) {
+        Scanner scanner = new Scanner(string);
+        String name = scanner.next();
+        int positive = Integer.parseInt(scanner.next());
+        int negative = Integer.parseInt(scanner.next());
+        if (positive < 0 || negative < 0)
+            return false;
+        circuit.addNode(positive);
+        circuit.addNode(negative);
+        double value = unitCalculator(scanner.next());
+        if (value <= 0)
+            return false;
+        if (positive != negative) {
+            switch (RLC) {
+                case 'R':
+                    circuit.addElement(name, positive, negative, "resistor", value);
+                    break;
+                case 'C':
+                    circuit.addElement(name, positive, negative, "capacitor", value);
+                    break;
+                case 'L':
+                    circuit.addElement(name, positive, negative, "inductance", value);
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static boolean addIndependentSource(Circuit circuit, String string, int IV) {
         Scanner scanner = new Scanner(string);
         String name = scanner.next();
         int positive = scanner.nextInt();
         int negative = scanner.nextInt();
+        if (positive < 0 || negative < 0)
+            return false;
         circuit.addNode(positive);
         circuit.addNode(negative);
         double offset = unitCalculator(scanner.next());
@@ -122,35 +167,16 @@ public class InputManager{
                 circuit.addElement(name, positive, negative, "independentVoltage", offset, amplitude, frequency, phase);
                 break;
         }
+        return true;
     }
 
-    public static void addRLC(Circuit circuit, String string, int RLC) {
-        Scanner scanner = new Scanner(string);
-        String name = scanner.next();
-        int positive = Integer.parseInt(scanner.next());
-        int negative = Integer.parseInt(scanner.next());
-        circuit.addNode(positive);
-        circuit.addNode(negative);
-        double value = unitCalculator(scanner.next());
-        switch (RLC) {
-            case 'R':
-                circuit.addElement(name, positive, negative, "resistor", value);
-                break;
-            case 'C':
-                circuit.addElement(name, positive, negative, "capacitor", value);
-                break;
-            case 'L':
-                circuit.addElement(name, positive, negative, "inductance", value);
-                break;
-        }
-
-    }
-
-    public static void addCurrentDependent(Circuit circuit, String string, int IV) {
+    public static boolean addCurrentDependent(Circuit circuit, String string, int IV) {
         Scanner scanner = new Scanner(string);
         String name = scanner.next();
         int positive = scanner.nextInt();
         int negative = scanner.nextInt();
+        if (positive < 0 || negative < 0)
+            return false;
         circuit.addNode(positive);
         circuit.addNode(negative);
         String elementDependedOn = scanner.next();
@@ -163,13 +189,16 @@ public class InputManager{
                 circuit.addElement(name, positive, negative, "CurrentDependentVoltage", elementDependedOn, gain);
                 break;
         }
+        return true;
     }
 
-    public static void addVoltageDependent(Circuit circuit, String string, int IV) {
+    public static boolean addVoltageDependent(Circuit circuit, String string, int IV) {
         Scanner scanner = new Scanner(string);
         String name = scanner.next();
         int positive = scanner.nextInt();
         int negative = scanner.nextInt();
+        if (positive < 0 || negative < 0)
+            return false;
         circuit.addNode(positive);
         circuit.addNode(negative);
         int positiveDependent = scanner.nextInt();
@@ -183,13 +212,16 @@ public class InputManager{
                 circuit.addElement(name, positive, negative, "voltageDependentVoltage", positiveDependent, negativeDependent, gain);
                 break;
         }
+        return true;
     }
 
-    public static void addDiode(Circuit circuit, String string) {
+    public static boolean addDiode(Circuit circuit, String string) {
         Scanner scanner = new Scanner(string);
         String name = scanner.next();
         int positive = scanner.nextInt();
         int negative = scanner.nextInt();
+        if (positive < 0 || negative < 0)
+            return false;
         circuit.addNode(positive);
         circuit.addNode(negative);
         int isIdeal = scanner.nextInt();
@@ -198,14 +230,48 @@ public class InputManager{
                 circuit.addElement(name, positive, negative, "diode");
                 break;
         }
+        return true;
+    }
+
+    public static boolean isEveryNumberValid(String string) {
+        Scanner scanner = new Scanner(string);
+        while (scanner.hasNext()){
+            if (!isNumberValid(scanner.next()))
+                return false;
+        }
+        return true;
+    }
+    public static boolean isNumberValid(String number){
+        char[] numberChar = number.toCharArray();
+        final char[] valids = {'G','M','k','m','u','n','p'};
+        char lastChar = numberChar[number.length() - 1];
+        int dotsNumber = 0;
+        if (numberChar[0] < '0' || numberChar[0] > '9')
+            return true;
+        for (int i = 0;i < number.length() - 1;i++){
+            if (numberChar[i] == '.')
+                dotsNumber++;
+            else if (numberChar[i] < '0' || numberChar[i] > '9')
+                return false;
+        }
+        if (dotsNumber > 1)
+            return false;
+        if (!(lastChar >= '0' && lastChar <= '9')){
+            boolean validFlag = false;
+            for (int i =0;i<valids.length;i++){
+                if (lastChar == valids[i])
+                    validFlag = true;
+            }
+            if (!validFlag)
+                return false;
+        }
+        return true;
     }
 
 
-
-
-
-
-
+    public boolean isTran() {
+        return flagTran;
+    }
     //practical method
     public static double unitCalculator(String dAmount) {
         int length = dAmount.length();
@@ -231,7 +297,7 @@ public class InputManager{
                 case 'p':
                     return number * 0.000000000001;
                 default:
-                    return -1;
+                    return 0;
             }
 
         }
--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,24 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-       //File input = new File("C:\\Users\\Asus\\IdeaProjects\\CircuitSimulator\\src\\input.txt");
-        File input = new File("C:\\input.txt");
+        //File input = new File("C:\\Users\\Asus\\IdeaProjects\\CircuitSimulator\\src\\input.txt");
+        File input = new File("D:\\input.txt");
         InputManager inputManager = new InputManager(input);
         Circuit circuit = inputManager.analyzeTheInput();
         //checks if dt,dv and di are all set and they are positive
         boolean tranFlag = inputManager.isTran();
-        if (!tranFlag)
-            System.out.println("Time is not given");
-        if (tranFlag){
 
-
-            int error=circuit.initializeGraph();
-            if(error!=0) {
-                System.out.println("Error " + error);
-                return;
-            }
-
-            circuit.solveCircuit();
-            System.out.println(circuit.toString());
-
-            // THE REST OF THE CODE MUST BEGIN FROM HERE
-
+      /*  int error = circuit.initializeGraph();
+        if (error != 0) {
+            System.out.println("Error " + error);
+            return;
         }
 
+        circuit.solveCircuit();
+        System.out.println(circuit.toString());
+
+        // THE REST OF THE CODE MUST BEGIN FROM HERE
+
+        */
     }
 }

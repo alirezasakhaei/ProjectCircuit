f514a2c5b09de74f699f3584e83eca6df83b73db
Asus <alirezasakhaeirad@gmail.com>
2020-07-29 00:51:17 +0430

    sss

--- src/Graphics.java
+++ src/Graphics.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -139,7 +140,7 @@ public class Graphics {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 if (isCircuitSolved) {
-
+                    dialogChooseElement();
                 }else {
                     JOptionPane.showMessageDialog(frame,"There is no circuit solved!");
                 }
@@ -148,13 +149,51 @@ public class Graphics {
 
         frame.setVisible(true);
     }
+    private void dialogChooseElement(){
+        JDialog dialogElement = new JDialog();
+        dialogElement.setLayout(null);
+        dialogElement.setBounds(0,0,500,500);
+        Container container = dialogElement.getContentPane();
+        container.setBackground(Color.RED.brighter().brighter());
+
+        int elementsNumber = circuit.getElements().size();
+        String[] elements = new String[elementsNumber];
+        int i = 0;
+        for (Map.Entry element : circuit.getElements().entrySet()){
+            elements[i] = ((Element) element.getValue()).name;
+            i++;
+        }
+
+        JComboBox comboBox = new JComboBox(elements);
+        comboBox.setBounds(200,150,100,20);
+        dialogElement.add(comboBox);
+
+        JLabel labelChoose = new JLabel("Choose the element!");
+        labelChoose.setFont(new Font("Arial",Font.BOLD,20));
+        labelChoose.setBounds(150,50,200,20);
+        dialogElement.add(labelChoose);
+
+        JButton buttonChoose = new JButton("Draw");
+        buttonChoose.setFont(new Font("Arial",Font.BOLD,10));
+        buttonChoose.setBounds(210,250,80,80);
+        dialogElement.add(buttonChoose);
+
+
+        buttonChoose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                dialogElement.setVisible(false);
+                draw(circuit.getElements().get((String) comboBox.getSelectedItem()));
+            }
+        });
+
+        dialogElement.setVisible(true);
+    }
     private void run(File input){
         if (input.canExecute()) {
             InputManager inputManager = new InputManager(input);
-            Circuit circuit = inputManager.analyzeTheInput();
+            circuit = inputManager.analyzeTheInput();
             Circuit.setCircuit(circuit);
-            CircuitPrinter circuitPrinter = new CircuitPrinter(circuit);
-            circuitPrinter.printData();
             ErrorFinder errorFinder = new ErrorFinder(circuit);
             int error = errorFinder.findErrors();
             if (error != 0) {
@@ -167,6 +206,12 @@ public class Graphics {
             }
         }else
             JOptionPane.showMessageDialog(frame, "File Not Executable!", "ERROR", JOptionPane.ERROR_MESSAGE);
+
+    }
+    private void draw(Element element){
+
+
+
 
     }
 
